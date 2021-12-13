de320cbf9a47f356f5df12e824b4d3f482a1268f
Davis Willard <dwillard@un-j4.kzn.21-school.ru>
2021-12-13 19:26:56 +0300

    init commit

---
+++ minishell.h
@@ -0,0 +1,15 @@
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <string.h>
+# include <unistd.h>
+# include <signal.h>
+
+#endif
