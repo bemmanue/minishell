40544398553114ecf1e4d2d82952cf7dc26c8463
Burma Emmanuel <bemmanue@ev-k4.kzn.21-school.ru>
2021-12-15 16:10:57 +0300

    1

--- sources/parser.c
+++ sources/parser.c
@@ -11,6 +11,15 @@ typedef struct s_command
 	int 		flag;
 }				t_command;
 
+void show_path(char **path)
+{
+	while (*path)
+	{
+		printf("%s\n", *path);
+		path++;
+	}
+}
+
 char **get_path(char **envp)
 {
 	char	**path;
@@ -20,26 +29,44 @@ char **get_path(char **envp)
 	i = 0;
 	while (envp[i])
 	{
-		if (!strncmp("PATH=", envp[i], 5))
+		if (!ft_strncmp("PATH=", envp[i], 5))
 			break ;
 		i++;
 	}
 	if (!envp[i])
 		return (NULL);
-	temp = envp[i] + 5;
+	temp = envp[i] + 6;
 	path = ft_split(temp, ':');
 	return (path);
 }
 
-t_command	*parse_string(char **envp, char **argv)
+
+t_command	*parse_string(char **envp, char *file)
 {
 	t_command	*command;
 	char		**path;
+	char		*absolute_path;
+	int 		i;
 
+	i = 0;
 	path = get_path(envp);
-	while ()
-
-
+	if (!access(file, F_OK))
+		printf("file is present\n");
+	else
+	{
+		file = ft_strjoin("/", file);
+		while (path[i])
+		{
+			absolute_path = ft_strjoin(path[i], file);
+			if (!access(absolute_path, F_OK))
+				printf("file is present\n");
+			free(absolute_path);
+			i++;
+		}
+	}
+	command = malloc(sizeof(t_command));
+	command->command = file;
+	command->flag = 0;
 	return (command);
 }
 
@@ -47,10 +74,13 @@ int	main(int argc, char **argv, char **envp)
 {
 	char *str;
 
+	if (argc != 1)
+		return (0);
+	(void)argv;
 	while (1)
 	{
 		str = readline("hello$ ");
-		parse_string(envp, argv);
+		parse_string(envp, str);
 		if (strlen(str) > 0)
 			add_history(str);
 	}
